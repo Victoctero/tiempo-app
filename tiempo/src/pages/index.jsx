80393a1f934016cf39ab85f8c1d6dede831e7cd5
Pedri <pedritociudad@hotmail.es>
2021-11-27 22:59:39 +0100

    card extra info a medias

--- tiempo/src/pages/index.jsx
+++ tiempo/src/pages/index.jsx
@@ -2,6 +2,7 @@ import DaySelector from '../components/day-selector';
 import fondo from '../images/fondo.jpg';
 import CardActualDay from '../components/card-actual-day';
 import SwitchNavigationContainer from '../components/switch-navigatio-container';
+import CardExtraInfo from '../components/card-extra-info';
 
 const page_container = {
     width: '100%',
@@ -16,6 +17,7 @@ export default function Page() {
             <SwitchNavigationContainer></SwitchNavigationContainer>
             <CardActualDay></CardActualDay>
             <DaySelector></DaySelector>
+            <CardExtraInfo></CardExtraInfo>
         </div>   
     )
 }
