e586cf2b48069bfbb65fff59fd2ed7b233afc4f6
MartinYela <martinyela@hotmail.com>
2021-11-29 20:10:28 +0100

    iconos en todas las cards, estilos en infocard, etc

--- tiempo/src/components/container-cards-extra-info/index.jsx
+++ tiempo/src/components/container-cards-extra-info/index.jsx
@@ -1,16 +1,20 @@
 import './style.css'
+import ClearskySun02 from '../../icons/ClearskySun02.svg'
 import CardExtraInfo from '../card-extra-info'
-
+import Windy12 from '../../icons/Windy12.svg'
+import Rainy11   from '../../icons/Rainy11.svg'
+import Humidity10   from '../../icons/Humidity10.svg'
+import Sunrise23   from '../../icons/Sunrise23.svg'
+import Sunsent24   from '../../icons/Sunsent24.svg'
 export default function ContainerCardsExtraInfo(props){
-
     return(
         <div className="cards__container">
-            <CardExtraInfo info = 'UV' datos = {props.datos.rayosUVI}></CardExtraInfo>
-            <CardExtraInfo info = 'VIENTO' datos = {props.datos.viento}></CardExtraInfo>
-            <CardExtraInfo info = 'LLUVIA' datos = {props.datos.lluvia}></CardExtraInfo>
-            <CardExtraInfo info = 'HUMEDAD' datos = {props.datos.humedad}></CardExtraInfo>
-            <CardExtraInfo info = 'AMANECER' datos = {props.datos.amanecer}></CardExtraInfo>
-            <CardExtraInfo info = 'ANOCHECER' datos = {props.datos.anochecer}></CardExtraInfo>
+            <CardExtraInfo info = 'UV' medida='' datos = {props.datos.rayosUVI} icon = {ClearskySun02}></CardExtraInfo>
+            <CardExtraInfo info = 'VIENTO' medida='Km/h' datos = {props.datos.viento} icon={Windy12}></CardExtraInfo>
+            <CardExtraInfo info = 'LLUVIA' medida='mm' datos = {props.datos.lluvia} icon={Rainy11}></CardExtraInfo>
+            <CardExtraInfo info = 'HUMEDAD' medida='%' datos = {props.datos.humedad} icon={Humidity10}></CardExtraInfo>
+            <CardExtraInfo  info = 'AMANECER' medida='H' datos = {props.datos.amanecer} icon={Sunrise23}></CardExtraInfo>
+            <CardExtraInfo info = 'ANOCHECER' medida='H' datos = {props.datos.anochecer} icon={Sunsent24}></CardExtraInfo>
         </div>
     )
 }
--- tiempo/src/components/day-selector/index.jsx
+++ tiempo/src/components/day-selector/index.jsx
@@ -72,7 +72,7 @@ export default function DaySelector(props) {
     // };
 
     return (
-        <Box sx={{ width: '100%', marginTop:'81px' }}>
+        <Box sx={{ width: '100%', marginTop:'81px', marginBottom:'57px' }}>
             <Tabs
                 value={value}
                 onChange={handleChange}
@@ -94,52 +94,52 @@ export default function DaySelector(props) {
             <Box sx={{ bgcolor: 'background.paper' }}>
                 <TabPanel value={value} index={0} dir={theme.direction} >
                     <div className="card__hour--container" >
-                        <CardHour temp = {props.datos.hourTemp0} hora = '00:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp1} hora = '01:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp2} hora = '02:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp3} hora = '03:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp4} hora = '04:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp5} hora = '05:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp6} hora = '06:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp7} hora = '07:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp8} hora = '08:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp9} hora = '09:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp10} hora = '10:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp11} hora = '11:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp12} hora = '12:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp13} hora = '13:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp14} hora = '14:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp15} hora = '15:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp16} hora = '16:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp17} hora = '17:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp18} hora = '18:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp19} hora = '19:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp20} hora = '20:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp21} hora = '21:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp22} hora = '22:00'></CardHour>
-                        <CardHour temp = {props.datos.hourTemp23} hora = '23:00'></CardHour>
+                        <CardHour temp={props.datos.hourTemp0}hora='00:00'icono={props.datos.hourIcon0}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp1} hora='01:00'icono= {props.datos.hourIcon1}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp2} hora='02:00'icono= {props.datos.hourIcon2}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp3} hora= '03:00'icono= {props.datos.hourIcon3}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp4} hora= '04:00'icono= {props.datos.hourIcon4}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp5} hora= '05:00'icono= {props.datos.hourIcon5}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp6} hora= '06:00'icono= {props.datos.hourIcon6}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp7} hora = '07:00'icono= {props.datos.hourIcon7}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp8} hora = '08:00' icono= {props.datos.hourIcon8}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp9} hora = '09:00' icono= {props.datos.hourIcon9}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp10} hora = '10:00'icono= {props.datos.hourIcon10}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp11} hora = '11:00'icono= {props.datos.hourIcon11}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp12} hora = '12:00'icono= {props.datos.hourIcon12}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp13} hora = '13:00'icono= {props.datos.hourIcon13}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp14} hora = '14:00'icono= {props.datos.hourIcon14}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp15} hora = '15:00'icono= {props.datos.hourIcon15}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp16} hora = '16:00'icono= {props.datos.hourIcon16}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp17} hora = '17:00'icono= {props.datos.hourIcon17}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp18} hora = '18:00'icono= {props.datos.hourIcon18}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp19} hora = '19:00'icono= {props.datos.hourIcon19}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp20} hora = '20:00'icono= {props.datos.hourIcon20}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp21} hora = '21:00'icono= {props.datos.hourIcon21}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp22} hora = '22:00'icono= {props.datos.hourIcon22}></CardHour>
+                        <CardHour temp = {props.datos.hourTemp23} hora = '23:00'icono= {props.datos.hourIcon23}></CardHour>
                         
                         
 
                     </div>
                 </TabPanel>
                 <TabPanel value={value} index={1} dir={theme.direction}>
-                    <CardMinMax></CardMinMax>
+                    <CardMinMax min={props.datos.minTemp1} max={props.datos.maxTemp1} icono={props.datos.iconoDay1}></CardMinMax>
                 </TabPanel>
                 <TabPanel value={value} index={2} dir={theme.direction}>
-                    <CardMinMax></CardMinMax>
+                    <CardMinMax min={props.datos.minTemp2} max={props.datos.maxTemp2} icono={props.datos.iconoDay2}></CardMinMax>
                 </TabPanel>
                 <TabPanel value={value} index={3} dir={theme.direction}>
-                    <CardMinMax></CardMinMax>
+                    <CardMinMax min={props.datos.minTemp3} max={props.datos.maxTemp3} icono={props.datos.iconoDay3}></CardMinMax>
                 </TabPanel>
                 <TabPanel value={value} index={4} dir={theme.direction}>
-                    <CardMinMax></CardMinMax>
+                    <CardMinMax min={props.datos.minTemp4} max={props.datos.maxTemp4} icono={props.datos.iconoDay4}></CardMinMax>
                 </TabPanel>
                 <TabPanel value={value} index={5} dir={theme.direction}>
-                    <CardMinMax></CardMinMax>
+                    <CardMinMax min={props.datos.minTemp5} max={props.datos.maxTemp5} icono={props.datos.iconoDay5}></CardMinMax>
                 </TabPanel>
                 <TabPanel value={value} index={6} dir={theme.direction}>
-                    <CardMinMax></CardMinMax>
+                    <CardMinMax min={props.datos.minTemp6} max={props.datos.maxTemp6} icono={props.datos.iconoDay6}></CardMinMax>
                 </TabPanel>
             </Box>
         </Box>
